0a37ec3362b80d3ac4a6757d37ef0d3d20bcfd1c
SidVaidya2005 <siddarthvaidya2005@gmail.com>
2026-04-08 16:23:15 +0530

    refactor: Move ScrollToTop component definition for better organization

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import "./index.css";
 
-function ScrollToTop() {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-}
-
 import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
@@ -23,6 +15,14 @@ import Navbar from "./landing_page/Navbar";
 import Footer from "./landing_page/Footer";
 
 const THEME_MODE_KEY = "zerodha-theme-mode";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function getSystemTheme() {
   return window.matchMedia("(prefers-color-scheme: dark)").matches
